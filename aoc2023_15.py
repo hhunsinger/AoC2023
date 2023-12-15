4312ea3fab8ff7611e47e4ff60feaa63f8804c38
Heather <mooflyfoof@Eds-MBP-3.localdomain>
2023-12-15 11:38:46 -0800

    turn HASH into function to prep for part 2

--- aoc2023_15.py
+++ aoc2023_15.py
@@ -2,7 +2,7 @@
 
 # test data
 # solution part 1 = 1320
-input_raw = 'rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7'
+input_raw = ['rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7']
 
 # actual data
 def load_input(file='private/aoc2023_15_input.txt'):
@@ -10,22 +10,30 @@ def load_input(file='private/aoc2023_15_input.txt'):
         input_array = myfile.read().splitlines()
     return input_array
 
-input_raw = load_input()
+# input_raw = load_input()
 
 steps = input_raw[0].split(',') # assuming input is only one line
+
+def hash_algo(char, val):
+    # Determine the ASCII code for the current character of the string.
+    ascii_code = ord(char)
+    # Increase the current value by the ASCII code you just determined.
+    val = val + ascii_code
+    # Set the current value to itself multiplied by 17.
+    val = val * 17
+    # Set the current value to the remainder of dividing itself by 256.
+    val = val % 256
+    return val
+
+# part 1
 current_vals = []
 for step in steps:
     # Current value starts at 0
     current_val = 0
     for char in step:
-        # Determine the ASCII code for the current character of the string.
-        ascii_code = ord(char)
-        # Increase the current value by the ASCII code you just determined.
-        current_val = current_val + ascii_code
-        # Set the current value to itself multiplied by 17.
-        current_val = current_val * 17
-        # Set the current value to the remainder of dividing itself by 256.
-        current_val = current_val % 256
+        current_val = hash_algo(char, current_val)
     current_vals.append(current_val) # could just sum it up as we go, but maybe we'll need them later
 
 print("answer part 1:", sum(current_vals)) # this was too easy
+
+# part 2
